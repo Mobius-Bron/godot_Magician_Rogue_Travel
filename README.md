239ed3765d1acfe5f739fc0b0572373c639d56c4
Mobius-Bron <156796273+Mobius-Bron@users.noreply.github.com>
2024-08-21 16:14:18 +0800

    添加测试用人物

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # godot_Magician_Rogue_Travel
+
+添加测试用早期角色，后期需要优化
---
+++ player_/player.gd
@@ -0,0 +1,25 @@
+extends CharacterBody2D
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var grahpic: Node2D = $Grahpic
+var SPEED = 100.0
+var direction
+
+func _physics_process(delta):
+	move()
+	animation_handler()
+	
+func move():
+	direction = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = direction * SPEED
+	if direction.x < 0:
+		grahpic.scale.x = -1
+	elif direction.x >0:
+		grahpic.scale.x = 1
+	move_and_slide()
+
+func animation_handler():
+	if direction.x == 0 and direction.y == 0:
+		animation_player.play("idle")
+	else:
+		animation_player.play("run")

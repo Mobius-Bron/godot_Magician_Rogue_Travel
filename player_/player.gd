2e7d40c17a69f3bc4ec2a12f008ae3f381346d89
Mobius-Bron <156796273+Mobius-Bron@users.noreply.github.com>
2024-08-24 22:06:04 +0800

    添加武器切换系统，需要改善

--- player_/player.gd
+++ player_/player.gd
@@ -2,10 +2,15 @@ extends CharacterBody2D
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var grahpic: Node2D = $Grahpic
+var weapon_dict: Dictionary = {0: preload("res://weapon/gun.tres"), 1: preload("res://weapon/fire_magic.tres")}
+var max_weapon_num = 2
+var now_weapon_num = 0
+
 var SPEED = 100.0
 var direction
 
 var weapon_root
+var weapon_node
 var weapon
 
 var mouse_position
@@ -13,11 +18,14 @@ var mouse_local_position
 
 func _ready():
 	weapon_root = $weapon_root
-	weapon = $weapon_root/weapon_node
+	weapon_node = $weapon_root/weapon_node
+	weapon = $weapon_root/weapon_node/weapon_test
+	print(weapon_dict)
 
 func _physics_process(delta):
 	move()
 	animation_handler()
+	weapon_change()
 	weapon_aim()
 	
 func move():
@@ -41,8 +49,15 @@ func weapon_aim():
 	mouse_local_position = get_local_mouse_position()
 	
 	if mouse_local_position.x > 0:
-		weapon.scale.y = 1
+		weapon_node.scale.y = 1
 	elif mouse_local_position.x < 0:
-		weapon.scale.y = -1
+		weapon_node.scale.y = -1
 	
 	weapon_root.look_at(mouse_position)
+	
+func weapon_change():
+	if Input.is_action_just_pressed("weapon_change"):
+		now_weapon_num = (now_weapon_num + 1) % max_weapon_num
+		#print(now_weapon_num,":",weapon_dict[now_weapon_num].get_position())
+		weapon.frame_coords = weapon_dict[now_weapon_num].get_position()
+		
---
+++ weapon/weapons.gd
@@ -0,0 +1,8 @@
+class_name Weapons extends Resource
+
+@export var Name: StringName
+@export var Damage: float
+@export var Position: Vector2
+
+func get_position():
+	return Position
